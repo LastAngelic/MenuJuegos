733ac70a30c85822eb1a41f8f6af3b3ef871db80
bryan-source <bescobar34@yahoo.com>
2022-05-02 13:22:21 -0500

    Containerbuttons

--- DetailScreen.js
+++ DetailScreen.js
@@ -6,7 +6,7 @@ function DetailScreen({navigation}) {
         <View style={{flex:1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'green'}}>
             <Text>Details Screen</Text>
 
-            <View>
+            <View style={styles.containerButton}>
                 <Button 
                     title="Atrás"
                     onPress={() => navigation.goBack()}
@@ -24,5 +24,20 @@ function DetailScreen({navigation}) {
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    containerButton: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        alignItems: 'center', 
+        backgroundColor: 'gray',
+
+        margin: 20,
+        padding: 5,
+
+        width: 300,
+        height: 100,
+    }
+})
 
 export default DetailScreen;
